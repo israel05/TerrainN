38c9cd6a96e991dbd5035a837025e225b5e67a8d
Israel de Lamo Blas <i.delamo@gmail.com>
2019-12-10 22:42:57 +0100

    Pantalla de inicio añadida y música

---
+++ terraintest2/Assets/CargaLaSiguiente.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CargaLaSiguiente : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 6f); // Espera dos segundos antes de lanzar la carga
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    
+}
