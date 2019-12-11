71cfcc494ebcb07abb7d5489c3ce8f8415c92be2
Israel de Lamo Blas <i.delamo@gmail.com>
2019-12-11 17:07:56 +0100

    Primer BUILD

--- terraintest2/Assets/scripts/Player.cs
+++ terraintest2/Assets/scripts/Player.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("En metros por segundo")][SerializeField] float xSpeed= 4f;
-    [Tooltip("desde el centro hasta salir de cámara")] [SerializeField] float posibleStrafe = 20f;
+    [Tooltip("En metros por segundo")][SerializeField] float xSpeed= 40f;
+    [Tooltip("desde el centro hasta salir de cámara")] [SerializeField] float posibleStrafe = 30f;
+    [Tooltip("En metros por segundo")] [SerializeField] float ySpeed = 40f;
+    [Tooltip("desde el centro hasta salir de cámara")] [SerializeField] float posibleVertical = 10f;
+    [SerializeField] float positionPitchFactor = 0.3f;
+    [SerializeField] float controlPitchFactor = -8f;
+    [SerializeField] float positionYawFactor = 4f;
+    [SerializeField] float controlYawFactor = -6f;
+    [SerializeField]float controlRollFactor = -6f;
+    private float xThrow;
+    private float yThrow;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +29,49 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed *Time.deltaTime;
-        float rawXNewXPos = transform.localPosition.x + xOffsetThisFrame;
-        float posicionRestringida = Mathf.Clamp(rawXNewXPos, -posibleStrafe, posibleStrafe);
-        transform.localPosition = new Vector3(
-            posicionRestringida, 
-            transform.localPosition.y, 
-            transform.localPosition.z);
+        MoverEnXeY();
+        MoverEnRotacion();
+
+    }
+
+    private void MoverEnRotacion()
+    {
+        float pitch= transform.localPosition.y * 
+            positionPitchFactor +yThrow * 
+            controlPitchFactor ;
+
+        float yaw = transform.localRotation.x *
+            positionYawFactor + xThrow *
+            controlYawFactor;
+        float roll = xThrow * controlRollFactor; 
+
+        //ajustar la rotación mediate quaternion euleriano
+       transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
         
+        
+    }
+
+    private void MoverEnXeY()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        float yOffsetThisFrame = yThrow * ySpeed * Time.deltaTime;
+
+
+        float rawXNewXPos = transform.localPosition.x + xOffsetThisFrame;
+        float rawYNewYPos = transform.localPosition.y + yOffsetThisFrame;
+
+
+        float posicionRestringidaX = Mathf.Clamp(rawXNewXPos, -posibleStrafe, posibleStrafe);
+        float posicionRestringidaY = Mathf.Clamp(rawYNewYPos, -posibleVertical, posibleVertical);
+
+
+        transform.localPosition = new Vector3(
+            posicionRestringidaX,
+            posicionRestringidaY,
+            transform.localPosition.z);
     }
 }
