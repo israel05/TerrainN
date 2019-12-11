48a2cad80eeae224644edccdc4028f9e8a2c23d2
Israel de Lamo Blas <i.delamo@gmail.com>
2019-12-11 15:14:49 +0100

    Waypath añadido

---
+++ terraintest2/Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("En metros por segundo")][SerializeField] float xSpeed= 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed *Time.deltaTime;
+        print(xOffsetThisFrame);
+        
+    }
+}
