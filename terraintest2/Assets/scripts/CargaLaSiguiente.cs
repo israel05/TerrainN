cecc2ed646a81ee361f3dadbd42483382949adb2
israel05 <i.delamo@gmail.com>
2019-12-24 15:02:34 +0100

    Proyecto reorganizado. Musica de inicio rota

--- terraintest2/Assets/scripts/CargaLaSiguiente.cs
+++ terraintest2/Assets/scripts/CargaLaSiguiente.cs
@@ -8,17 +8,11 @@ public class CargaLaSiguiente : MonoBehaviour
 {
 
 
-    //creo un objeto antes de todo con awake
-    //creo un objeto permanetne a todo el juego
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoadFirstScene", 6f); // Espera dos segundos antes de lanzar la carga
+        Invoke("LoadFirstScene", 10f); // Espera dos segundos antes de lanzar la carga
     }
 
     void LoadFirstScene()
